c1c08ab448cc02fa1eb2299198681971d8fee52a
Koushik Biswas <koushik.biswas@cloudreach.com>
2020-06-05 01:21:44 +0000

    first commit - gke cluster & node pool

---
+++ gke-cluster-nodepool/main.tf
@@ -0,0 +1,34 @@
+provider "google" {
+        project         	= "cr-lab-kbiswas-2304204819"
+	version			= "~> 3.20"
+	region			= "us-central1"
+	zone			= "us-central1-c"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "my-gke-cluster"
+  location = "us-central1"
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  location   = "us-central1"
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-1"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
